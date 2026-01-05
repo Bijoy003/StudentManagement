836af9c703d3c11623507c28a3a8b3368bef3821
Bhugol Chakma <bhugol.chakma@enosisbd.com>
2026-01-05 17:44:54 +0600

    Remove Domain from main repo (prepare for submodule)

--- StudentManagement.Domain/Entities/Course.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentManagement.Domain.Entities
-{
-    public class Course
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Credits { get; set; }
-    }
-}
